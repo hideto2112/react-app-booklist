78d35466b525f71cf0d1a8f749b8e406b0d016e8
Hideto Ando <39573967+hideto2112@users.noreply.github.com>
2020-05-25 00:25:13 +0900

    Updates

---
+++ precache-manifest.41107a0d5a53c1965c7e2bb104f61799.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2244812563a9fd7bc96d724c88ce65c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "950d9697319a5de0e8e8",
+    "url": "/static/js/2.9e9f95cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9e9f95cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d8c6c05e262507c276",
+    "url": "/static/js/main.ebbf4566.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
