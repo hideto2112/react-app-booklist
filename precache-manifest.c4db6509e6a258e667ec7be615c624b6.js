aaa420c821824f5878e31405c418a203b5b66e47
Hideto Ando <39573967+hideto2112@users.noreply.github.com>
2020-05-28 06:33:10 +0900

    Updates

---
+++ precache-manifest.c4db6509e6a258e667ec7be615c624b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318aef8efcc4d1741a52628d3fa02b6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73dd3b4985727d69f77d",
+    "url": "/static/js/2.6a962681.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6a962681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e66509a71377f9765fc3",
+    "url": "/static/js/main.5413d549.chunk.js"
+  },
+  {
+    "revision": "4f0bad368f348ac3998f",
+    "url": "/static/js/runtime-main.7614d3cf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app-booklist/precache-manifest.3415b44201b94cbb9039997c46369e6b.js"
+  "/precache-manifest.c4db6509e6a258e667ec7be615c624b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app-booklist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
